aedd8ace3f73ae5275be007653953232f4c597f4
Rango <wangqiang@smzdm.com>
2019-06-27 18:37:18 +0800

    基本框架搭建完成，等待nsvUserInfo的Behavior的实现

--- app/src/main/java/com/apeman/rxlifedemo/HomeActivity.kt
+++ app/src/main/java/com/apeman/rxlifedemo/HomeActivity.kt
@@ -4,24 +4,38 @@ package com.apeman.rxlifedemo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
+import io.github.heartinfei.slogger.Configuration
+import io.github.heartinfei.slogger.S
+import io.github.heartinfei.slogger.plan.DebugPlan
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
+import kotlinx.android.synthetic.main.activity_user_home.*
 import java.util.concurrent.TimeUnit
 
-class HomeActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(p0: View?) {
-
-    }
-
+class HomeActivity : AppCompatActivity() {
     private val lifecycleSubject = BehaviorSubject.create<ReleaseEvent>()
 
     @SuppressWarnings("all")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_home)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        S.init(Configuration()).addPlans(DebugPlan())
+        S.i(nsvUserInfo.measuredHeight.toString())
+        nsvUserInfo.post {
+            S.i("ScrollView 高度：" + nsvUserInfo.measuredHeight.toString())
+            S.i("最大滚动距离：" + nsvUserInfo.maxScrollAmount)
+            S.i("collapsingToolbar高度：" + collapsingToolbar.measuredHeight.toString())
+            S.i("coordinator 高度：" + coordinator.measuredHeight)
+            S.i("appBarLayout 高度：" + appBarLayout.measuredHeight)
+            collapsingToolbar.layoutParams.height = coordinator.measuredHeight - tabLayout.measuredHeight
+            appBarLayout.layoutParams.height = coordinator.measuredHeight
+        }
+    }
+
+    private fun rxTest() {
         Log.i("wangqiang", "onCreate")
         Observable.interval(1, TimeUnit.SECONDS).map {
             throw RuntimeException("Error")
